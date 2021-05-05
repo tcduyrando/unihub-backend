61fe8f83ce9085aea7c9b10e0510a774d1ede56e
tcduyrando <s3757330@rmit.edu.vn>
2021-05-05 15:06:59 +0700

    4th. added CrossOrigin

--- src/main/java/com/example/demo/controller/FavoriteController.java
+++ src/main/java/com/example/demo/controller/FavoriteController.java
@@ -15,6 +15,7 @@ import org.hibernate.SQLQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Favorite;
 import com.example.demo.repository.FavoriteRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/")
 public class FavoriteController {
--- src/main/java/com/example/demo/controller/SchoolController.java
+++ src/main/java/com/example/demo/controller/SchoolController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.School;
 import com.example.demo.repository.SchoolRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/")
 public class SchoolController {
